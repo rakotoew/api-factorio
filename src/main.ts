e0781f5eb8bf140c301216f703783cc15f1450e1
Ewan Rakotoanosy <rakotoew@users.noreply.github.com>
2024-01-17 11:21:10 +0100

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+      .setTitle('Factorio API')
+      .setDescription('Api for factorio items and craft (Vanilla for the time being')
+      .setVersion('1.0')
+      .addTag('factorio')
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document)
   await app.listen(3000);
 }
 bootstrap();
